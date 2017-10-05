636dad12976ebbbfcb1be6f39de6d8709be86eaa
Ubuntu <subangkit@ONE.a511xvlxnceermgpdp4xlpe3wh.hx.internal.cloudapp.net>
2017-10-05 02:13:35 +0000

    Update and and install yarn

--- install_nvm.sh
+++ install_nvm.sh
@@ -1,5 +1,5 @@
-sudo apt-get update && sudo apt-get install build-essential libssl-dev && curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash && source ~/.profile
-nvm ls-remote
+sudo apt-get update && sudo apt-get install build-essential libssl-dev && cd ~ && curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+echo 'run source ~/.profile'
 echo 'nvm install <latest_version>'
 echo 'nvm alias default <latest_version>'
 echo 'reopen ssh'
